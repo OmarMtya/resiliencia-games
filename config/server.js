224af223fe5a82fd1a4469b57b2231fc1dac7039
Omar Montoya <luis.montoya.j@gmail.com>
2021-09-01 11:20:21 -0600

    Cambios en servidor para produccion

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1339),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '612fd3a7e9379944b12ba6dab2f79257'),
