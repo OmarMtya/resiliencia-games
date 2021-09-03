e7170b2cb7dd55149d370cc4b16532a4a5ae4eb4
Omar Montoya <luis.montoya.j@gmail.com>
2021-09-03 11:15:35 -0600

    Cambio de puerto

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1339),
+  port: env.int('PORT', 1340),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '612fd3a7e9379944b12ba6dab2f79257'),
